b2ea72b7002aaa858c6eb9ff483fe70dac16646e
geekbrains <info@geekbrains.ru>
2022-07-01 21:39:51 +0400

    43_edit

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -10,6 +10,9 @@ double y1Test = k1 * 0.1 + b1;
 double y2Test = k2 * 0.1 + b2;
 double div1 = y1 - y2;
 double div2 = y1Test - y2Test;
+double calculateStep = 1;
+int calculateStepsCount = 6;
+
 if(div1 < 0)
 {
     div1 = div1 * -1;
@@ -18,6 +21,7 @@ if(div2 < 0)
 {
     div2 = div2 * -1;
 }
+
 if(y1 == y2)
 {
     Console.WriteLine("Точка пересечения прямых на координате: (" + x1 + "; " + y1 + ")");
@@ -28,55 +32,106 @@ else
     {  
         if(y1 < y2)
         {
-            while(y1 < y2)
+            for(int i = 0; i < calculateStepsCount; i++)
             {
-                x1 = x1 - 0.1;
-                x2 = x2 - 0.1;
-                y1 = k1 * x1 + b1;
-                y2 = k2 * x2 + b2;
-            }
+                while(y1 < y2)
+                {
+                    x1 = x1 - calculateStep;
+                    x2 = x2 - calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                }
+                if(i != calculateStepsCount)
+                {
+                    x1 = x1 + calculateStep;
+                    x2 = x2 + calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                    calculateStep = calculateStep / 10;
+                }
+            }    
         }
         else
         {
-            while(y1 > y2)
+            for(int i = 0; i < calculateStepsCount; i++)
             {
-                x1 = x1 - 0.1;
-                x2 = x2 - 0.1;
-                y1 = k1 * x1 + b1;
-                y2 = k2 * x2 + b2;
+                while(y1 > y2)
+                {
+                    x1 = x1 - calculateStep;
+                    x2 = x2 - calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                }
+                if(i != calculateStepsCount)
+                {
+                    x1 = x1 + calculateStep;
+                    x2 = x2 + calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                    calculateStep = calculateStep / 10;
+                }
             }
         }
-        Console.WriteLine("Точка пересечения прямых на координате: (" + x1 + "; " + y1 + ")");
     }
     if(div1 > div2)
     {
         if(y1 < y2)
         {
-            while(y1 < y2)
+            for(int i = 0; i < calculateStepsCount; i++)
             {
-                x1 = x1 + 0.1;
-                x2 = x2 + 0.1;
-                y1 = k1 * x1 + b1;
-                y2 = k2 * x2 + b2;
-            }
+                while(y1 < y2)
+                {
+                    x1 = x1 + calculateStep;
+                    x2 = x2 + calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                }
+                if(i != calculateStepsCount)
+                {
+                    x1 = x1 - calculateStep;
+                    x2 = x2 - calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                    calculateStep = calculateStep / 10;
+                }
+            } 
         }
         else
         {
-            while(y1 > y2)
+            for(int i = 0; i < calculateStepsCount; i++)
             {
-                x1 = x1 + 0.1;
-                x2 = x2 + 0.1;
-                y1 = k1 * x1 + b1;
-                y2 = k2 * x2 + b2;
+                while(y1 > y2)
+                {
+                    x1 = x1 + calculateStep;
+                    x2 = x2 + calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                }
+                if(i != calculateStepsCount)
+                {
+                    x1 = x1 - calculateStep;
+                    x2 = x2 - calculateStep;
+                    y1 = k1 * x1 + b1;
+                    y2 = k2 * x2 + b2;
+                    calculateStep = calculateStep / 10;
+                }
             }
         }
-        Console.WriteLine("Точка пересечения прямых на координате: (" + x1 + "; " + y1 + ")");
     }
 }
 
+double xAns = Approximate(x1);
+double yAns = Approximate(y1);
+Console.WriteLine("Точка пересечения прямых на координате: (" + xAns + "; " + yAns + ")");
 
 int InputData(string output)
 {
     Console.Write(output);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+double Approximate(double a)
+{
+    a = Math.Round(a, 3);
+    return a;
 }
