eb7303d9c57ff44d3bf5034a78e743ebc966967c
geekbrains <info@geekbrains.ru>
2022-07-14 14:37:55 +0400

    edit complete

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -17,16 +17,20 @@ string bufer = string.Empty;
 int j = 0;
 for(int i = 0; i < input.Length; i++)
 {
-    while(input[i] != ',' && i != input.Length)
+    while(input[i] != ',' && i != input.Length)  // Второе условие не выполняется и индекс уходит в зашкал с вылетом ошибки(если без if(break))
     {
         bufer = bufer + input[i].ToString();
         Console.WriteLine("Bufer: " + bufer);
         i++;
+        if(i == input.Length) // Необходимо, чтобы принудительно завершить цикл после достижения максимального индекса
+        {
+            break;
+        }
     }
     array[j] = Convert.ToInt32(bufer);                      //\
     Console.WriteLine("Array[j] value: " + array[j]);       // \
-    j++;                                                    //  } ЭТА ЧАСТЬ КОДА НЕ ВЫПОЛНЯЕТСЯ НА ПОСЛЕДНЕМ ВИТКЕ ЦИКЛА 'FOR' или 'while' И ПРОГА КРАШИТСЯ
-    bufer = string.Empty;                                   // /
+    j++;                                                    //  } ЭТА ЧАСТЬ КОДА НЕ ВЫПОЛНЯЕТСЯ НА ПОСЛЕДНЕМ ВИТКЕ ЦИКЛА 'while' И ПРОГА КРАШИТСЯ, без прерывателя if(break),
+    bufer = string.Empty;                                   // /  то есть второе условие после && не выполняется
 }
 PrintArray(array);
 int counter = 0;
